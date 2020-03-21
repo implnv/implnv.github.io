cd1c730034f8691e090c89f5ae849fb4f394be06
implnv <49292707+implnv@users.noreply.github.com>
2020-03-21 14:23:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,6 @@ VK.init({
 btn.addEventListener('click', () => {
     VK.Auth.login((response) => {
         if (response.session) {
-            let xhr = new XMLHttpRequest();
-            let url = new URL('https://login.vk.com/?act=openapi&oauth=1&aid=7368594&location=implnv.github.io&new=1');
-            xhr.open('POST', url);
-            xhr.send();
-            console.log(xhr);
-            
             // VK.Api.call('fave.get', { item_type: 'post', v: '5.103' }, (response) => {
             //     console.log(response);
             // });
