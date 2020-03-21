6aac9cd52a0b8623b64b40487f0a328be0287559
implnv <49292707+implnv@users.noreply.github.com>
2020-03-21 13:11:11 -0700

    1

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ VK.init({
     apiId: 7368594
 });
 
-VK.Auth.login((response) => {
-    if (response.session) {
-        VK.Api.call('fave.get', { item_type: "post", v: "5.103" }, (response) => {
-            console.log(response);
-        });
-    }
+btn.addEventListener('click', () => {
+    VK.Auth.login((response) => {
+        if (response.session) {
+            VK.Api.call('fave.get', { item_type: 'post', v: '5.103' }, (response) => {
+                console.log(response);
+            });
+        }
+    });
 });
