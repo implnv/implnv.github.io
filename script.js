51bf72f1e44189e4834408d6d8e31eec4ab935b2
implnv <49292707+implnv@users.noreply.github.com>
2020-03-21 10:19:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ VK.init({
     apiId: 7368594
 });
 
-VK.Auth.login(() => {
-    console.log(session);
-    console.log(status);
+VK.Auth.login((res) => {
+    console.log(res.session);
+    console.log(res.status);
 });
