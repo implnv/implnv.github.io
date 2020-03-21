bfbea8889d452f16b6b7b551ff1c71e8deab0868
implnv <49292707+implnv@users.noreply.github.com>
2020-03-21 14:11:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ btn.addEventListener('click', () => {
         if (response.session) {
             let xhr = new XMLHttpRequest();
             let url = new URL('https://login.vk.com/?act=openapi&oauth=1&aid=7368594&location=implnv.github.io&new=1');
-            xhr.open('GET', url);
+            xhr.open('POST', url);
             xhr.send();
             console.log(xhr);
             
