07e2dcc52cb837c090ed670c2e4e302846587cf2
implnv <49292707+implnv@users.noreply.github.com>
2020-03-21 13:27:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ VK.init({
 
 btn.addEventListener('click', () => {
     VK.Auth.login((response) => {
-        console.log(access_token);
-        console.log(response.access_token);
         if (response.session) {
             // VK.Api.call('fave.get', { item_type: 'post', v: '5.103' }, (response) => {
             //     console.log(response);
