23015c32610d77b4115a203cb61ca5b8af0b2ca3
implnv <49292707+implnv@users.noreply.github.com>
2020-03-21 15:16:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,7 @@ VK.init({
 });
 
 btn.addEventListener('click', () => {
-    const newLocal = 1 << 7;
     VK.Auth.login((response) => {
-        console.log(response);
         if (response.session) {
             VK.Api.call('photos.getAll', { owner_id: 238741249, v: '5.103' }, (response) => {
                 console.log(response);
