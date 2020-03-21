4ef83b097c53ba60d88f28c81e2424ff934aad60
implnv <49292707+implnv@users.noreply.github.com>
2020-03-21 14:06:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,9 @@ btn.addEventListener('click', () => {
     VK.Auth.login((response) => {
         if (response.session) {
             let xhr = new XMLHttpRequest();
-            let url = new URL('https://login.vk.com');
-            url.searchParams.set('act', 'openapi', 'oauth', '1');
+            let url = new URL('https://login.vk.com/?act=openapi&oauth=1&aid=7368594&location=implnv.github.io&new=1');
             console.log(url);
-            
-            xhr.open('GET', 'act=openapi&oauth=1&aid=7368594&location=implnv.github.io&new=1');
+            xhr.open('GET', url);
             xhr.send();
             console.log(xhr.response);
             
